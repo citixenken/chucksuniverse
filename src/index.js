fff2f52e2d16a0d0091c13b0987446ae05b6a7c9
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-06-22 06:20:06 +0300

    event listeners for rating component

--- src/index.js
+++ src/index.js
@@ -63,3 +63,45 @@ const randomize = document.getElementById("reload-joke");
 randomize.addEventListener("click", () => {
   getRandomJoke();
 });
+
+// RATING IMPLEMENTATION
+let ratDisplay = document.querySelector(".rating");
+let stars = document.querySelectorAll(".fa-star-o");
+let totalStar = 0;
+
+stars.forEach((star, index) => {
+  star.dataset.rating = index + 1;
+  star.addEventListener("mouseover", onMouseOver);
+  star.addEventListener("click", onClick);
+  star.addEventListener("mouseleave", onMouseLeave);
+});
+
+function onMouseOver(e) {
+  const ratingVal = e.target.dataset.rating;
+  if (!ratingVal) {
+    return;
+  } else {
+    fill(ratingVal);
+  }
+}
+
+function fill(ratingVal) {
+  for (let i = 0; i < 5; i++) {
+    if (i < ratingVal) {
+      stars[i].classList.replace("fa-star-o", "fa-star");
+    } else {
+      stars[i].classList.replace("fa-star", "fa-star-o");
+    }
+  }
+}
+
+function onMouseLeave(e) {
+  fill(totalStar);
+}
+
+function onClick(e) {
+  const ratingVal = e.target.dataset.rating;
+  totalStar = ratingVal;
+  fill(totalStar);
+  ratDisplay.innerHTML = ratingVal;
+}
