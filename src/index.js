b54033118cf72031d7eb9bb364863150b9df4446
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-06-22 04:07:13 +0300

    implemented Randomize button functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   getRandomJoke();
+  getRandomJokeSpecifyCategory();
+  showCategories();
+  searchJokes();
 });
 
 function parseResponse(response) {
@@ -10,15 +13,53 @@ function handleError(error) {
   throw error;
 }
 
-const BASE_URL = "https://api.chucknorris.io/jokes/random";
+const BASE_URL = "https://api.chucknorris.io/jokes/";
+const jokesRandom = "random";
+const jokesCategories = "categories";
+let category = "dev";
+const jokesRandomSpecifyCategory = `random?category=${category}`;
+let query = "nft";
+const queryJokes = `search?query=${query}`;
 
 function getRandomJoke() {
-  fetch(BASE_URL)
+  fetch(BASE_URL + `${jokesRandom}`)
     .then(parseResponse)
-    .then((joke) => {
-      //   console.log(joke);
-      let jokeContent = document.querySelector(".value");
-      jokeContent.innerText = joke.value;
+    .then((randomJoke) => {
+      let jokeContent = document.querySelector(".joke-content");
+      jokeContent.innerText = randomJoke.value;
     })
     .catch(handleError);
 }
+
+function getRandomJokeSpecifyCategory() {
+  fetch(BASE_URL + `${jokesRandomSpecifyCategory}`)
+    .then(parseResponse)
+    .then((randomJokeSpecifyCategory) => {
+      //   console.log(randomJokeSpecifyCategory);
+    })
+    .catch(handleError);
+}
+
+function showCategories() {
+  fetch(BASE_URL + `${jokesCategories}`)
+    .then(parseResponse)
+    .then((jokeCategories) => {
+      //   console.log(jokeCategories);
+    })
+    .catch(handleError);
+}
+
+function searchJokes() {
+  fetch(BASE_URL + `${queryJokes}`)
+    .then(parseResponse)
+    .then((searchJokes) => {
+      console.log(searchJokes);
+    })
+    .catch(handleError);
+}
+
+// RANDOMIZE BUTTON FUNCTIONALITY
+const randomize = document.getElementById("reload-joke");
+randomize.addEventListener("click", () => {
+  getRandomJoke();
+});
